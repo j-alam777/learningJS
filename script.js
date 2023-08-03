2a9265f09b6642fec24cd728c844bcb86c3a202b
j-alam777 <j_alam777@yahoo.com>
2023-08-03 14:03:04 +0600

    added

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 // document.write("Hello World");
-let userInput1 = prompt ();
+let userInput1 = prompt();
 let userInput2 = prompt();
-document.write(userInput1);
-document.write(userInput2);
+// document.write(userInput1);
+// document.write(userInput2);
+
+userInput1 = parseInt (userInput1);
+userInput2 = parseInt (userInput2);
+
+const Result = userInput1 + userInput2;
+
+document.write ("Output:" + Result );
